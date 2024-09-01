d5983dcf2f3db15b5e72519f8627c38d5880958a
Ivan Egorov <me@egori.ch>
2024-09-01 16:15:27 +0000

    wip: Iteration over lispm/lrt0 done
    Submitting the current progress, the lispm/lrt0 binaries build alright,
    the rest does not yet build.

--- lispm.h
+++ lispm.h
@@ -59,13 +59,6 @@ extern __attribute__((noreturn)) void lispm_rt_abort(void);
  * -    <N:28> 00 11: builtin function at the offset N in builtins table;
  *
  * -       ... 11 11: special values
- *         0000 000F: T
- *         FFFF FFDF: used during evaluation of capture lists of LET;
- *                    marks a literal first defined in the previous part of
- *                    the LET evaluations;
- *         FFFF FFEF: used during evaluation of capture lists of LAMBDA/LET;
- *                    marks a literal already in the capture list;
- *         FFFF FFFF: no value currently associated with the literal.
  */
 typedef unsigned Sym;
 
@@ -82,7 +75,7 @@ typedef unsigned Sym;
 struct __attribute__((aligned(16))) Builtin {
   const char *name;
   Sym (*eval)(Sym args);
-  Sym (*evcap)(Sym args, Sym caps);
+  Sym (*parse)(void);
   Sym *store; /* if non-NULL, the registered literal is stored into this location */
 };
 /* Convenience macro to implement extensions */
@@ -98,8 +91,6 @@ struct Lispm {
   Sym *stack;
   /* Stack pointer. Grows down. */
   Sym *sp;
-  /* Parser pointer. Grows up, must be at least 4 words above stack bottom. */
-  Sym *pp;
 
   /* Beginning of the strings storage. */
   char *strings;
@@ -124,15 +115,19 @@ struct Lispm {
   /* Internal values */
   unsigned htable_index_size;
   int htable_index_shift;
+  Sym parse_frame;
+  Sym frame_shadow;
+  unsigned frame_depth;
 };
 
-/* The very bottom of the stack can be used for special purposes.
-   This value defines the size of that bottom part. */
-#define LISPM_PP_OFFSET 64u
+enum {
+  /* Hash table uses open addressing.
+     This value limits how many slots are looked up before we give up. */
+  LISPM_STRINGS_INDEX_LOOKUP_LIMIT = 32u,
 
-/* Hash table uses open addressing.
-   This value limits how many slots are looked up before we give up. */
-#define STRINGS_INDEX_LOOKUP_LIMIT 32u
+  /* The bottom of the stack is used to communicate information about errors. */
+  LISPM_STACK_BOTTOM_OFFSET = 8u,
+};
 
 /* External symbols that runtime must provide: */
 /* Invokes `fn` such that a call to `lispm_rt_throw()` causes immediate stack unwinding
@@ -152,10 +147,10 @@ static inline int lispm_is_power_of_two(unsigned i) { return i && !(i & (i - 1))
 static inline int lispm_shortnum_can_represent(unsigned val) { return (val & LISPM_UPPER_BITS(2)) == 0; }
 static inline int lispm_is_valid_config(void) {
   const struct Lispm *m = &lispm;
-  return m->stack + LISPM_PP_OFFSET <= m->pp && m->pp < m->sp /**/
-         && m->strings + 8 <= m->strings_end                  /**/
-         && m->program <= m->pc && m->pc < m->program_end     /**/
-         && m->htable + 1024 <= m->htable_end                 /**/
+  return m->stack + LISPM_STACK_BOTTOM_OFFSET < m->sp     /**/
+         && m->strings + 8 <= m->strings_end              /**/
+         && m->program <= m->pc && m->pc < m->program_end /**/
+         && m->htable + 1024 <= m->htable_end             /**/
          && lispm_is_power_of_two(m->htable_end - m->htable)
          /* we also want all offsets to fit into short unsigned */
          && lispm_shortnum_can_represent(m->sp - m->stack)            /**/
@@ -241,7 +236,7 @@ static inline Sym lispm_shortnum_mul(Sym p, Sym q, int *overflow) {
 
 /* stack objects */
 enum {
-  LISPM_ST_OBJ_CONS   = 2u,
+  LISPM_ST_OBJ_CONS = 2u,
   LISPM_ST_OBJ_LAMBDA = 6u,
 
   /* an extension object, taking TWO words on stack */
@@ -299,12 +294,8 @@ Sym lispm_builtin_as_sym(const struct Builtin *bi);
 
 enum {
   LISPM_SYM_NIL = 0,
-  LISPM_SYM_T   = LISPM_MAKE_BUILTIN_SYM(0),
+  LISPM_SYM_T = LISPM_MAKE_BUILTIN_SYM(0),
   LISPM_SYM_ERR = LISPM_MAKE_BUILTIN_SYM(1),
-
-  LISPM_SYM_NO_ASSOC = LISPM_MAKE_SPECIAL_VALUE(0),
-  LISPM_SYM_BOUND    = LISPM_MAKE_SPECIAL_VALUE(1),
-  LISPM_SYM_FREE     = LISPM_MAKE_SPECIAL_VALUE(2),
 };
 
 /* Internal API */
@@ -320,7 +311,7 @@ struct LispmTraceCallbacks {
   void (*oom_htable)(const char *file, unsigned line);
   void (*oom_strings)(const char *file, unsigned line);
   void (*unbound_symbol)(const char *file, unsigned line, Sym sym);
-  void (*illegal_bind)(const char *file, unsigned line, Sym sym, Sym assoc);
+  void (*illegal_bind)(const char *file, unsigned line, Sym sym);
 };
 
 #if LISPM_CONFIG_VERBOSE
@@ -345,8 +336,10 @@ extern struct LispmTraceCallbacks lispm_trace;
 __attribute__((noreturn)) void lispm_panic(Sym ctx);
 
 /* pc must be between M.program and M.program_end */
-Sym lispm_parse(const char *pc, const char *pc_end);
+Sym lispm_parse_quote(const char *pc, const char *pc_end);
 Sym lispm_eval(const char *pc, const char *pc_end);
+
+unsigned lispm_list_scan(Sym* out, Sym li, unsigned limit);
 
 Sym lispm_st_obj_alloc(unsigned k, Sym *vals);
 static inline Sym lispm_cons_alloc(Sym car, Sym cdr) {
@@ -355,8 +348,3 @@ static inline Sym lispm_cons_alloc(Sym car, Sym cdr) {
 }
 
 Sym *lispm_st_obj_unpack(Sym s);
-Sym *lispm_cons_unpack_user(Sym a);
-
-Sym lispm_evcap_quote(Sym a, Sym c);
-Sym lispm_evquote(Sym a);
-void lispm_args_unpack2(Sym a, Sym *f, Sym *s);
--- lrt0.c
+++ lrt0.c
@@ -3,10 +3,7 @@
 
 #define M lispm
 
-enum {
-  PAGE_PROGRAM = 0,
-  PAGE_STRINGS = 1,
-};
+enum { PAGE_PROGRAM = 0, PAGE_STRINGS = 1 };
 
 static void check_access_read(unsigned access, Sym ptr) {
   LISPM_EVAL_CHECK(access & PAGE_ACCESS_R, ptr, panic, "read access denied: ", ptr);
@@ -17,14 +14,14 @@ static void check_access_write(unsigned access, Sym ptr) {
 static Sym lispm_literal_name_span(Sym s) {
   LISPM_ASSERT(lispm_sym_is_literal(s));
   unsigned offs = lispm_literal_str_offs(s);
-  unsigned len  = __builtin_strlen(M.strings + offs);
+  unsigned len = __builtin_strlen(M.strings + offs);
   return lispm_span_alloc(lispm_make_span(PAGE_STRINGS, offs, len));
 }
 static void *lispm_span_base(struct Span span, unsigned *len, unsigned *access) {
   void *ptr, *b, *e;
   unsigned page = lispm_shortnum_val(span.page);
-  *len          = lispm_shortnum_val(span.len);
-  *access       = PAGE_ACCESS_R;
+  *len = lispm_shortnum_val(span.len);
+  *access = PAGE_ACCESS_R;
   switch (page) {
   case PAGE_PROGRAM:
     b = (void *)M.program, e = (void *)M.program_end;
@@ -48,38 +45,44 @@ static Sym PROGRAM(Sym e) {
   LISPM_EVAL_CHECK(lispm_sym_is_nil(e), e, panic, "no arguments expected, got: ", e);
   return lispm_span_alloc(lispm_make_span(PAGE_PROGRAM, 0, M.program_end - M.program));
 }
-static Sym GETC(Sym ptr) {
-  ptr = lispm_evquote(ptr);
+static Sym GETC(Sym args) {
+  Sym ptr;
+  LISPM_EVAL_CHECK(lispm_list_scan(&ptr, args, 1) == 1 && lispm_sym_is_span(ptr), args, panic,
+                   "one span argument expected, got: ", args);
   unsigned len, access;
-  struct Span span       = lispm_span_unpack_user(ptr);
+  struct Span span = lispm_span_unpack(ptr);
   const unsigned char *c = lispm_span_base(span, &len, &access);
   check_access_read(access, ptr);
   if (!len) return LISPM_SYM_NIL; /* position right after the interval */
   return lispm_make_shortnum(*c);
 }
-static Sym COPY(Sym a) {
-  Sym d, s;
-  lispm_args_unpack2(a, &d, &s);
-  struct Span dest = lispm_span_unpack_user(d), src = lispm_span_unpack_user(s);
+static Sym COPY(Sym args) {
+  Sym ds[2];
+  LISPM_EVAL_CHECK(lispm_list_scan(ds, args, 2) == 2 && lispm_sym_is_span(ds[0]) && lispm_sym_is_span(ds[1]), args,
+                   panic, "two span argument expected, got: ", args);
+  struct Span dest = lispm_span_unpack(ds[0]), src = lispm_span_unpack(ds[1]);
   unsigned dest_len, dest_access, src_len, src_access;
   void *dest_ptr = lispm_span_base(dest, &dest_len, &dest_access),
-       *src_ptr  = lispm_span_base(src, &src_len, &src_access);
-  LISPM_EVAL_CHECK(src_len <= dest_len, a, panic, "copy out of bounds for args: ", a);
-  check_access_read(src_access, s);
-  check_access_write(dest_access, d);
+       *src_ptr = lispm_span_base(src, &src_len, &src_access);
+  LISPM_EVAL_CHECK(src_len <= dest_len, args, panic, "copy out of bounds for args: ", args);
+  check_access_read(src_access, ds[0]);
+  check_access_write(dest_access, ds[1]);
   __builtin_memcpy(dest_ptr, src_ptr, src_len);
   dest.len = lispm_make_shortnum(src_len);
   return lispm_span_alloc(dest);
 }
-static Sym STR(Sym e) {
-  e = lispm_evquote(e);
-  LISPM_EVAL_CHECK(lispm_sym_is_literal(e), e, panic, "literal expected, got: ", e);
-  return lispm_literal_name_span(e);
+static Sym STR(Sym args) {
+  Sym lit;
+  LISPM_EVAL_CHECK(lispm_list_scan(&lit, args, 1) == 1 && lispm_sym_is_literal(lit), args, panic,
+                   "one literal argument expected, got: ", args);
+  return lispm_literal_name_span(lit);
 }
-static Sym CHARS(Sym ptr) {
-  ptr = lispm_evquote(ptr);
+static Sym CHARS(Sym args) {
+  Sym ptr;
+  LISPM_EVAL_CHECK(lispm_list_scan(&ptr, args, 1) == 1 && lispm_sym_is_span(ptr), args, panic,
+                   "one span argument expected, got: ", args);
   unsigned len, access;
-  const unsigned char *base = lispm_span_base(lispm_span_unpack_user(ptr), &len, &access);
+  const unsigned char *base = lispm_span_base(lispm_span_unpack(ptr), &len, &access);
   check_access_read(access, ptr);
   Sym res = LISPM_SYM_NIL;
   for (const unsigned char *p = base + len; p != base;)
@@ -87,18 +90,19 @@ static Sym CHARS(Sym ptr) {
   return res;
 }
 static Sym SPAN(Sym args) {
-  Sym span, begin, end, *cons;
-  cons = lispm_cons_unpack_user(args), span = cons[0], begin = cons[1];
-  cons = lispm_cons_unpack_user(begin), begin = cons[0], end = cons[1];
-  if (!lispm_sym_is_nil(end)) end = lispm_evquote(end);
-  struct Span res   = lispm_span_unpack_user(span);
+  Sym sbe[3] = {}; /* span, begin, end */
+  unsigned argc = lispm_list_scan(sbe, args, 3);
+  LISPM_EVAL_CHECK((argc == 2 || argc == 3) && lispm_sym_is_span(sbe[0]) && lispm_sym_is_shortnum(sbe[1]) &&
+                       (lispm_sym_is_nil(sbe[2]) || lispm_sym_is_shortnum(sbe[2])),
+                   args, panic, "span, begin[, end] arguments expected, got: ", args);
+  struct Span res = lispm_span_unpack_user(sbe[0]);
   unsigned src_offs = lispm_shortnum_val(res.offs);
-  unsigned src_len  = lispm_shortnum_val(res.len);
+  unsigned src_len = lispm_shortnum_val(res.len);
 
-  unsigned res_begin = lispm_shortnum_val(begin);
-  unsigned res_end   = !lispm_sym_is_nil(end) ? lispm_shortnum_sval(end) : src_len;
+  unsigned res_begin = lispm_shortnum_val(sbe[1]);
+  unsigned res_end = !lispm_sym_is_nil(sbe[2]) ? lispm_shortnum_sval(sbe[2]) : src_len;
   if (((int)res_end) < 0) res_end = src_len + res_end;
-  LISPM_EVAL_CHECK(res_begin <= res_end && res_end <= src_len, span, panic, "subspan does not fit: ", span);
+  LISPM_EVAL_CHECK(res_begin <= res_end && res_end <= src_len, sbe[0], panic, "subspan does not fit: ", sbe[0]);
 
   /* we rely on the fact that all LISP-visible span constructors allocate shortnum-pages,
      and all operations can only shrink their input spans,
@@ -106,22 +110,24 @@ static Sym SPAN(Sym args) {
      although we do indirectly assert-check.
   */
   res.offs = lispm_make_shortnum(src_offs + res_begin);
-  res.len  = lispm_make_shortnum(res_end - res_begin);
+  res.len = lispm_make_shortnum(res_end - res_begin);
   return lispm_span_alloc(res);
 }
-static void lispm_program_get(Sym ptr, const char **begin, const char **end) {
-  ptr                    = lispm_evquote(ptr);
-  const struct Span span = lispm_span_unpack_user(ptr);
+static void lispm_program_get(Sym args, const char **begin, const char **end) {
+  Sym ptr;
+  LISPM_EVAL_CHECK(lispm_list_scan(&ptr, args, 1) == 1 && lispm_sym_is_span(ptr), args, panic,
+                   "one span argument expected, got: ", args);
+  const struct Span span = lispm_span_unpack(ptr);
   LISPM_EVAL_CHECK(lispm_shortnum_val(span.page) == PAGE_PROGRAM, ptr, panic,
                    "parsing non-program page is prohibited, got: ", ptr);
   unsigned access, len;
   *begin = lispm_span_base(span, &len, &access);
-  *end   = *begin + len;
+  *end = *begin + len;
 }
-static Sym PARSE(Sym ptr) {
+static Sym PARSE(Sym args) {
   const char *begin, *end;
-  lispm_program_get(ptr, &begin, &end);
-  return lispm_parse(begin, end);
+  lispm_program_get(args, &begin, &end);
+  return lispm_parse_quote(begin, end);
 }
 static Sym IMPORT(Sym ptr) {
   /* the important difference from eval, is that parsing happens in-place,
@@ -135,17 +141,16 @@ static Sym IMPORT(Sym ptr) {
 LISPM_BUILTINS_EXT(LRT0_SYMS) = {{"MODULO"}};
 
 typedef enum { OP_OR, OP_AND, OP_XOR, OP_ADD, OP_SUB, OP_MUL } BinOp;
-static int binop_unpack(Sym a, int arith, unsigned *p, unsigned *q) {
-  Sym *pq = lispm_cons_unpack_user(a);
-  Sym *qm = lispm_cons_unpack_user(pq[1]);
-  *p = pq[0], *q = qm[0];
-  if (lispm_sym_is_nil(qm[1])) return 0;
-  LISPM_EVAL_CHECK(arith, a, panic, "exactly two operands expected, got: ", a);
-
-  Sym m = lispm_evquote(qm[1]);
-  LISPM_EVAL_CHECK(m == lispm_builtin_as_sym(LRT0_SYMS), m, panic,
-                   "only MODULO is allowed as third argument, got: ", m);
-  return 1;
+static int binop_unpack(Sym args, int arith, unsigned *p, unsigned *q) {
+  Sym pqm[3] = {};
+  const unsigned argc = lispm_list_scan(pqm, args, arith ? 3 : 2);
+  LISPM_EVAL_CHECK(((argc == 2) || (arith && argc == 3)) && lispm_sym_is_shortnum(pqm[0]) &&
+                       lispm_sym_is_shortnum(pqm[1]) &&
+                       (lispm_sym_is_nil(pqm[2]) || lispm_builtin_as_sym(LRT0_SYMS) == pqm[2]),
+                   args, panic, "p, q[, modulo] expected, got: ", args);
+  *p = lispm_shortnum_val(pqm[0]);
+  *q = lispm_shortnum_val(pqm[1]);
+  return lispm_builtin_as_sym(LRT0_SYMS) == pqm[2];
 }
 static Sym binop(Sym args, BinOp op, int arith) {
   Sym p, q;
@@ -182,14 +187,16 @@ static Sym ADD(Sym a) { return binop(a, OP_ADD, 1); }
 static Sym SUB(Sym a) { return binop(a, OP_SUB, 1); }
 static Sym MUL(Sym a) { return binop(a, OP_MUL, 1); }
 
-static Sym BNOT(Sym val) {
-  val = lispm_evquote(val);
-  LISPM_EVAL_CHECK(lispm_sym_is_shortnum(val), val, panic, "numeric value expected, got: ", val);
+static Sym BNOT(Sym args) {
+  Sym val;
+  LISPM_EVAL_CHECK(lispm_list_scan(&val, args, 1) == 1 && lispm_sym_is_shortnum(val), args, panic,
+                   "numeric value expected, got: ", args);
   return lispm_shortnum_bitwise_not(val);
 }
-static Sym NEG(Sym val) {
-  val = lispm_evquote(val);
-  LISPM_EVAL_CHECK(lispm_sym_is_shortnum(val), val, panic, "numeric value expected, got: ", val);
+static Sym NEG(Sym args) {
+  Sym val;
+  LISPM_EVAL_CHECK(lispm_list_scan(&val, args, 1) == 1 && lispm_sym_is_shortnum(val), args, panic,
+                   "numeric value expected, got: ", args);
   return lispm_shortnum_neg(val);
 }
 
